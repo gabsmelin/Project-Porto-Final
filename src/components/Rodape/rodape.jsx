0261e5d010b802cb2b262d5feb22fd242cd45143
gabsmelin <rm550202@fiap.com.br>
2023-11-14 22:26:02 -0300

    Rodapé feito e estilzado.

---
+++ src/components/Rodape/rodape.jsx
@@ -0,0 +1,11 @@
+import './rodape.scss'
+
+export default function Rodape() {
+    return(
+        <div className='container-rodape'>
+           <p>Na Guincho Seguro, estamos comprometidos em fornecer serviços de guincho confiáveis e profissionais 24 horas por dia, 7 dias por semana. Somos a sua solução de confiança quando você mais precisa, esteja você preso na estrada ou enfrentando uma emergência veicular. Sua segurança e satisfação são nossa prioridade número um. Estamos prontos para atender às suas necessidades de guincho de forma rápida e eficiente, para que você possa voltar à estrada com tranquilidade.</p>
+           <p>©Copyright 2023 Guincho Seguro. Todos os direitos reservados</p>
+        </div>
+    )
+}
+
