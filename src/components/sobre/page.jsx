7fb46c2b718a8de9f804f1d8d9a2cf8513b678d6
Gustavoferreiraaraujo <gustavoferreira620@gmail.com>
2023-11-14 22:46:24 -0300

    Adicionando alt nas img

--- src/components/sobre/page.jsx
+++ src/components/sobre/page.jsx
@@ -5,12 +5,12 @@ export default function Sobre() {
     return(
         <div className="container-sobre">
             <div className="parte-one">
-                <Image src="/img/barraDeitada.png" alt="" width={1500} height={2}/>
+                <Image src="/img/barraDeitada.png" alt="Barra deitada." width={1500} height={2}/>
                 <div className='conteudo'>
                     <h1>NOSSO PROPÓSITO É SER UM GUINCHO CONFIÁVEL PARA VOCÊ</h1>
-                    <Image src="/img/imgSobre.png" alt="" width={400} height={600}/>
+                    <Image src="/img/imgSobre.png" alt="Imagem que nos retrata." width={400} height={600}/>
                 </div>
-                <Image src="/img/barraDeitada.png" alt="" width={1500} height={1000}/>    
+                <Image src="/img/barraDeitada.png" alt="Barra deitada." width={1500} height={1000}/>    
             </div>
             <div className="parte-two">
                 <h3>Somos a companhia que está com você em todos os momentos da vida</h3>
